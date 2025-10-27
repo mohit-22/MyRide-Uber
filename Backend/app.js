28f010c12771ce2413046c234015411853a9ebbc
mohit-22 <mohitgarghnd825@gmail.com>
2025-10-27 16:48:58 +0530

    make user model

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,37 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const cors = require('cors')
+// const express = require('express')
+// const app = express()
+// const connectToDb = require('./db/db.js');
+// connectToDb();
+
+// app.use(cors());
+
+// app.get('/' , (req,res) => {
+//     res.send("Hello World")
+// })
+
+// module.exports = app
+
+
+
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors')
-const express = require('express')
-const app = express()
+
+console.log('🌍 DB_CONNECT from env:', process.env.DB_CONNECT); // 👈 ye line add karo
+
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const connectToDb = require('./db/db.js');
+connectToDb();
 
 app.use(cors());
 
-app.get('/' , (req,res) => {
-    res.send("Hello World")
-})
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
 
-module.exports = app
+module.exports = app;
