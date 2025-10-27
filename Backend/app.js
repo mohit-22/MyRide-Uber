b5a90a444ea4d77866ec14dfbeafe74685b21d64
mohit-22 <mohitgarghnd825@gmail.com>
2025-10-27 17:02:14 +0530

    make user controller , user routes, user service

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,13 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
-// const cors = require('cors')
-// const express = require('express')
-// const app = express()
-// const connectToDb = require('./db/db.js');
-// connectToDb();
-
-// app.use(cors());
-
-// app.get('/' , (req,res) => {
-//     res.send("Hello World")
-// })
-
-// module.exports = app
-
-
-
 const dotenv = require('dotenv');
 dotenv.config();
 
-console.log('🌍 DB_CONNECT from env:', process.env.DB_CONNECT); // 👈 ye line add karo
 
 const cors = require('cors');
 const express = require('express');
 const app = express();
-
 const connectToDb = require('./db/db.js');
+const userRoutes = require('./routes/user.routes');
+
 connectToDb();
 
 app.use(cors());
@@ -33,5 +15,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
